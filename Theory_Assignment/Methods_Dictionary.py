f43cdb30601f4f1e955dab7ec078920fc065637c
may <mayumiph28@gmail.com>
2021-07-11 14:53:30 +0100

    Example of methods dictionary added

--- Theory_Assignment/Methods_Dictionary.py
+++ Theory_Assignment/Methods_Dictionary.py
@@ -1,65 +1,128 @@
 #clear
 print('Example clear')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+playlist.clear()
+print(playlist)
 print()
 
 #copy
 print('Example copy')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+playlist.copy()
+print(playlist)
 print()
 
 #fromkeys
 print('Example fromkeys')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = ("artist","song","duration")
+details = 0
+playlist_dict = dict.fromkeys(playlist,details)
+print(playlist_dict)
 print()
+
+#details = ("Jorja Smith", "Be Honest", "3:27")
 
 #get
 print('Example get')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+song = playlist.get("song")
+print(song)
 print()
 
 #items
 print('Example items')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+items = playlist.items()
+print(items)
 print()
 
 #keys
 print('Example keys')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+keys = playlist.keys()
+print(keys)
 print()
 
 #pop
 print('Example pop')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+which_artist = playlist.pop("song")
+print(which_artist)
+print()
+
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+playlist.pop("song")
+print(playlist)
 print()
 
 #popitem
 print('Example popitem')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+playlist.popitem()
+print(playlist)
 print()
 
 #setdefault
 print('Example setdefault')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+artist = playlist.setdefault("artist")
+print(artist)
 print()
 
 #update
 print('Example update')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+feat = playlist.update({"feat":"Burna Boy"})
+print(playlist)
 print()
 
 #values
 print('Example values')
-name = str('MAYUmI').capitalize()
-print(name)
+playlist = {
+    "artist": "Jorja Smith",
+    "song": "Be Honest",
+    "duration": "3:27"
+}
+data = playlist.values()
+print(data)
 print()
